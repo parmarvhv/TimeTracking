0600bf7f7c6a4fc3eb6cb3d38f3d31d0b3a609cc
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-11-18 15:17:15 +0530

    initilize project

---
+++ ProjectFiles/Code/UI/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTracking
+//
+//  Created by Vaibhav Parmar on 18/11/17.
+//  Copyright © 2017 Vaibhav Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
